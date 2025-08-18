7cb26eafbda88082e0fbc86221a4587388c546d9
suriyak2001 <suriyak2030@gmail.com>
2025-08-18 19:22:07 +0530

    classes and objects

--- Demo.java
+++ Demo.java
@@ -1,7 +1,17 @@
-class Demo
-{
-  public static void main (String [] args) {
-    System.out.println("A for allen");
-    System.out.println("B for barky");
+class Demo{
+  public static void main(String [] args) {
+
+    int number1 = 15;
+    int number2 = 25;
+    Calculator calc = new Calculator();
+    int result = calc.add(number1, number2);
+    System.out.println(result);
+
+  }
+}
+
+class Calculator {
+  public int add(int n1, int n2){
+    return n1 + n2;
   }
 }
