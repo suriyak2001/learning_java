ce7f11aa4d2935c592e1cf2c267bfbe69c7e1109
suriyak2001 <suriyak2030@gmail.com>
2025-08-23 22:12:11 +0530

    practicing datatypes in java

--- Gfg.java
+++ Gfg.java
@@ -8,5 +8,23 @@ class Gfg{
     } else {
       System.out.println("you are below 50");
     }
+    CheckBooleans();
+    FloatsInJava();
+  }
+
+  public static void CheckBooleans() {
+    boolean b1 = true;
+    boolean b2 = false;
+
+    System.out.println("is java really fun?" + b1);
+    System.out.println("is fish tasty?" + b2);
+  }
+
+  public static void FloatsInJava() {
+    float pi = 3.14f;
+    float gravity = 9.81f;
+
+    System.out.println("value of pi is " + pi);
+    System.out.println("value of gravity is " + gravity);
   }
 }
