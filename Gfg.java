d76c368d889b5fe4cf5ad8c074de7f6ee0f394b4
suriyak2001 <suriyak2030@gmail.com>
2025-08-23 21:53:52 +0530

    creating a class where we can filter the age by using control statements

---
+++ Gfg.java
@@ -0,0 +1,12 @@
+class Gfg{
+  public static void main(String[] args) {
+    final int age = 43;
+
+    // using if-else statement
+    if (age > 50) {
+      System.out.println("you are above 50");
+    } else {
+      System.out.println("you are below 50");
+    }
+  }
+}
