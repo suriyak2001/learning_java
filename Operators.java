b975e51a2ecedec2206fa74ff94b7a248b7fc289
suriyak2001 <suriyak2030@gmail.com>
2025-08-24 09:49:41 +0530

    arithmetic operators

--- Operators.java
+++ Operators.java
@@ -1,4 +1,4 @@
-public class Operators{
+/*public class Operators{
   public static void main(String[] args) {
     int a = 10;
     int b = 5;
@@ -25,5 +25,32 @@ public class Operators{
 
     System.out.println(result);
 
+  }
+}*/
+
+class Operators{
+  public static void  main (String[] args) {
+    //arithmetic operators
+
+    //integers
+    int a = 45;
+    int b = 35;
+
+    //strings
+    String n1 = "67";
+    String n2 = "46";
+
+    //convert string to integers
+    int a1 = Integer.parseInt(n1);
+    int b1 = Integer.parseInt(n2);
+
+    arithmetic(a, b, a1, b1);
+    
+  }
+  public static void arithmetic(int a, int b, int a1, int b1) {
+    System.out.println("sum: " + (a+b));
+    System.out.println("sub: " + (a-b));
+    System.out.println("multiply: " + (a1*b1));
+    System.out.println("reminder: " + (a1%b1));
   }
 }
