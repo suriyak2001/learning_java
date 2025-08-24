7431bd72db312e83e65d51c5d4bcabff42428784
suriyak2001 <suriyak2030@gmail.com>
2025-08-24 11:58:56 +0530

    unary and logical operators

--- Operators.java
+++ Operators.java
@@ -30,7 +30,6 @@
 
 class Operators{
   public static void  main (String[] args) {
-    //arithmetic operators
 
     //integers
     int a = 45;
@@ -44,13 +43,36 @@ class Operators{
     int a1 = Integer.parseInt(n1);
     int b1 = Integer.parseInt(n2);
 
+    //booleans
+    boolean boo1 = true;
+    boolean boo2 = false;
+
     arithmetic(a, b, a1, b1);
+    Unary(a, b);
+    Logical(boo1, boo2);
     
   }
+
+  //arithmetic operators
   public static void arithmetic(int a, int b, int a1, int b1) {
     System.out.println("sum: " + (a+b));
     System.out.println("sub: " + (a-b));
     System.out.println("multiply: " + (a1*b1));
     System.out.println("reminder: " + (a1%b1));
+  }
+
+  //unary operator
+  public static void Unary(int a, int b){
+    System.out.println("pre-increment: " + (++a));
+    System.out.println("post-increment: " + (a++));
+    System.out.println("pre-decrement: " + (--b));
+    System.out.println("post-decrement: " + (b--));
+  }
+
+  //logical operators
+  public static void Logical(boolean boo1, boolean boo2){
+    System.out.println("if both the condition are true: " + (boo1 && boo2));
+    System.out.println("if any one of the condition is true: " + (boo1 || boo2));
+    System.out.println("used to reverse the condition: " + (!boo1));
   }
 }
